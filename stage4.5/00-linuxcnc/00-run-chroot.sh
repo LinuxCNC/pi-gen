25a8f2de17552878a7f25792084f27ae1b3ae235
Jeff Epler <jepler@unpythonic.net>
2020-01-11 10:10:59 -0600

    additional setup

--- stage4.5/00-linuxcnc/00-run-chroot.sh
+++ stage4.5/00-linuxcnc/00-run-chroot.sh
@@ -11,8 +11,15 @@ rm -f /tmp/linuxcnc.deb /tmp/linuxcnc-dev.deb /tmp/kernel.deb
 
 # Default to the RT kernel
 printf '[all]\ninclude config-rt.txt\n' >> /boot/config.txt
-printf '[pi4]\nkernel=vmlinuz-4.19.71-rt24-v7l+\n' >> /boot/config-rt.txt
+printf '[pi4]\nkernel=vmlinuz-4.19.71-rt24-v7l+\nforce_turbo=1\n' >> /boot/config-rt.txt
 
 # Add necessary kernel configuration
 sed -i  '1s/$/ isolcpus=3 idle=poll/' /boot/cmdline.txt
 
+# Do raspi-configgy things
+# Sam reports the "desktop" gl driver gives better performance
+raspi-config nonint do_gldriver G2
+
+# 0 means enabled, based on UTSL (because it's a UNIX exit status, where 0 means success)
+raspi-config nonint do_spi 0
+raspi-config nonint do_net_names 0
