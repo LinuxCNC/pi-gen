5ba329f67ee1fd2b25523d464e66e5a9f69793f6
Jeff Epler <jepler@unpythonic.net>
2020-05-01 15:30:02 -0500

    filenames are important

--- stage0/99-linuxcnc-kernel/00-run-chroot.sh
+++ stage0/99-linuxcnc-kernel/00-run-chroot.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+apt remove raspberrypi-kernel || true
+apt install -y wget
+
 # TODO get kernel from a proper apt repository
 wget -O /tmp/kernel.deb https://media.unpythonic.net/pi4-kernel-pkg/linux-image-4.19.71-rt24-v7l+_4.19.71-rt24-v7l+-4_armhf.deb
 
